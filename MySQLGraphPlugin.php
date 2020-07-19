5e8e045d4395993050418b67ebc34a40b7af556d
jerome <psic@free.fr>
2020-07-20 00:40:08 +0200

    clean

--- MySQLGraphPlugin.php
+++ MySQLGraphPlugin.php
@@ -25,28 +25,27 @@ class MySQLGraphPlugin extends AbstractPicoPlugin
 	 */
 	public function onConfigLoaded(array &$config)
 	{
-	$this->config = include('MySQLConfig.php'); 
-	if (isset($config['mysql_source']))
+        $this->config = include('MySQLConfig.php'); 
+        if (isset($config['mysql_source']))
         {
             $db_conf = $config['mysql_source'];
             $i=0;
 
-	    foreach ($this->config as $db_config_key => $db_config_array)
-	    {
-		$one_db_conf = $db_conf[$db_config_key];
-		foreach($one_db_conf as $query_name => $query_string)
-		{
-			$this->config[$db_config_key][$query_name] = $query_string;
-			$i++;
-		}
-	    } 
+            foreach ($this->config as $db_config_key => $db_config_array)
+            {
+                $one_db_conf = $db_conf[$db_config_key];
+                foreach($one_db_conf as $query_name => $query_string)
+                {
+                    $this->config[$db_config_key][$query_name] = $query_string;
+                    $i++;
+                }
+            } 
         }
-		
 	}
 	
     public function onContentPrepared(&$content)
     {
-              // Search for Embed shortcodes allover the content
+        // Search for Embed shortcodes allover the content
         preg_match_all('#\[db_graph *.*?\]#s', $content, $matches);
 
         // Make sure we found some shortcodes
@@ -60,60 +59,56 @@ class MySQLGraphPlugin extends AbstractPicoPlugin
                 if ( ! preg_match('/graph=[\"]([^\"]*)[\"]/', $match, $graph))
                     $error = true;
                 preg_match('/title=[\"]([^\"]*)[\"]/', $match, $title); 
-		preg_match('/height=[\"]([^\"]*)[\"]/', $match, $height);
-		preg_match('/width=[\"]([^\"]*)[\"]/', $match, $width);
-		preg_match('/settings=[\"]([^\"]*)[\"]/', $match, $settings_conf);
-		preg_match('/is_data_column=[\"]([^\"]*)[\"]/', $match, $column);
+                preg_match('/height=[\"]([^\"]*)[\"]/', $match, $height);
+                preg_match('/width=[\"]([^\"]*)[\"]/', $match, $width);
+                preg_match('/settings=[\"]([^\"]*)[\"]/', $match, $settings_conf);
+                preg_match('/is_data_column=[\"]([^\"]*)[\"]/', $match, $column);
                 if (! $error)
                 {
- 		      $query_string="";
-                      $db_name_string="";
-                      $found = 0;
-                      foreach ($this->config as $db_name => $db_conf_array)
-                      {
-                          foreach($db_conf_array as $key => $value)
-                          {
-                                  if($key == $query[1])
-                                  {
-                                          $query_string = $value;
-                                          $found = 1;
-                                  }
-                          }
-                          if($found == 1)
-                                  $db_name_string = $db_name;
-                      }
-  			if(strtoupper(substr(trim($query_string),0,6) ) != 'SELECT')
-			{
+                    $query_string="";
+                    $db_name_string="";
+                    $found = 0;
+                    foreach ($this->config as $db_name => $db_conf_array)
+                    {
+                        foreach($db_conf_array as $key => $value)
+                         {
+                            if($key == $query[1])
+                            {
+                                    $query_string = $value;
+                                    $found = 1;
+                            }
+                        }
+                        if($found == 1)
+                           $db_name_string = $db_name;
+                    }
+                    if(strtoupper(substr(trim($query_string),0,6) ) != 'SELECT')
+                    {
                     	    $content = preg_replace('#\[db_graph *.*?\]#s', '*MySQLGraph ERROR*', $content, 1);
-			}
-			else
-			{
-			    if($column != null)
-				    $is_column = $column[1];
-			    else
-				    $is_column = 1;
+                    }
+                    else
+                    {
+                        if($column != null)
+                            $is_column = $column[1];
+                        else
+                            $is_column = 1;
 
-			     $result = $this->makeQuery($db_name_string,$query_string,$is_column );
-			    // Replace embeding code with the shortcode in the content
-			    $settings = array();
-			//	 'back_colour' => 'white',
-			//	  'graph_title' => 'Start of Fibonacci series'
-			//	);
-			    if ($title != null)
-				    $settings['graph_title']=$title[1];
-			    if ($settings_conf != null)
-			    {
-				    $settings = json_decode(str_replace('\'','"',$settings_conf[1]),true);
-			    }
-			    if($width != null && $height != null)
-				$graphR = new Goat1000\SVGGraph\SVGGraph($width[1], $height[1],$settings);
-			    else
-				$graphR = new Goat1000\SVGGraph\SVGGraph(640, 480,$settings);
-//			    $graphR->colours(['red','green','blue']);
-			    $graphR->values($result);
-			    $content = preg_replace('#\[db_grap *.*?\]#s',  $graphR->fetch($graph[1], false), $content, 1);
-			    //$content = preg_replace('#\[db_grap *.*?\]#s',  $graphR->fetch('PieGraph', false), $content, 1);
-			}
+                        $result = $this->makeQuery($db_name_string,$query_string,$is_column );
+                        // Replace embeding code with the shortcode in the content
+                        $settings = array();
+                        if ($title != null)
+                            $settings['graph_title']=$title[1];
+                        if ($settings_conf != null)
+                        {
+                            $settings = json_decode(str_replace('\'','"',$settings_conf[1]),true);
+                        }
+                        if($width != null && $height != null)
+                            $graphR = new Goat1000\SVGGraph\SVGGraph($width[1], $height[1],$settings);
+                        else
+                            $graphR = new Goat1000\SVGGraph\SVGGraph(640, 480,$settings);
+        //			    $graphR->colours(['red','green','blue']);
+                        $graphR->values($result);
+                        $content = preg_replace('#\[db_grap *.*?\]#s',  $graphR->fetch($graph[1], false), $content, 1);
+                    }
                 }
                 else
                     $content = preg_replace('#\[db_graph *.*?\]#s', '*MySQLGraph ERROR*', $content, 1);
